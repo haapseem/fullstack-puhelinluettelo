7b0a9078cb5ed931d1b2ff477d78c0f5a709d278
Eemil Haapsaari <eemil.haapsaari@vismappg.fi>
2019-02-03 23:49:04 +0200

    express

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :j
 
 app.use(express.static('front/build'))
 
+
 var persons = [
     {
       name: "Arto Hellas",
@@ -79,6 +80,7 @@ app.get('/info', (req, res) => {
 
 
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
